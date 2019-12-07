c8d86f6bab726bbdd7c2591c263716b5e7a3c787
manuaraujo15 <manu.ff.araujo15@gmail.com>
2019-12-07 18:59:04 -0500

    inclusão do laço For

--- olamundo.c
+++ olamundo.c
@@ -2,6 +2,12 @@
 int main()
 {
     printf("Ola Mundoooooo!\n");
-    printf("Meu primeiro código");
-    printf("Meu primeiro código em C usando versionamento Git");
+    printf("Meu primeiro código\n");
+    printf("Meu primeiro código em C usando versionamento Git\n");
+
+    // for(i=0;i > 100; i++){
+    //     printf("Ola Mundoooooo!\n");
+    //     printf("Meu primeiro código");
+    //     printf("Meu primeiro código em C usando versionamento Git");
+    // }
 }
